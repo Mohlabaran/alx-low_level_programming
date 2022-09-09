a53b25fcf3769b99313bc5744bb33c15e31039d9
Mohlabaran <Ultimatelabsy@gmai.com>
2022-09-09 13:20:47 -0700

    update on shell permission

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 /**
  *main -is the entry point for the program.
  *
- *Return:0 if no error, Non-zero value  if errors.
+ *Return:0 if no errors, return non zero if errors
  */
 int main(void)
 {
@@ -11,5 +11,5 @@ int main(void)
 	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %ld byte(s)\n", sizeof(float));
-	return (0)
+	return (0);
 }
