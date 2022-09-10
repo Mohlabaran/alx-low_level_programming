3e1efe6d33a1027d8caa207850f3b196d9bc9035
Mohlabaran <Ultimatelabsy@gmai.com>
2022-09-09 20:49:50 -0700

    update on shell permission

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,17 +10,17 @@ int main(void)
 	for (num = '0'; num <= '9'; num++)
 	{
 		putchar(num);
-	}
-	if (num == '9')
-	{
-		break;
-	}
-	else
-	{
+		if (num == '9')
+		{
+			break;
+		}
+		else
+		{		
 		putchar(',');
 		putchar(' ');
+		}
 	}
 	putchar('\n');
-	return (0);
+	return (0);}
 }
 
