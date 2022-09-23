1c68e2c6b8119829d041004249ef44e9a1e054ec
Mohlabaran <Ultimatelabsy@gmai.com>
2022-09-22 17:48:33 -0700

    update on shell permission

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,18 +1,30 @@
 #include "main.h"
 /**
- * _strncpy - copies a string
+ * _strncat - concatenates two strings,
  * @dest: destination.
  * @src: source.
- * @n: amount of bytes from src.
+ * @n: amount of bytes used from src.
  * Return: the pointer to dest.
  */
-char *_strncpy(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 	int i;
+	int j;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-	dest[i] = src[i];
-	for ( ; i < n; i++)
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 	dest[i] = '\0';
+
 	return (dest);
 }
